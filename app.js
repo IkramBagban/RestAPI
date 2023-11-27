8df05bf0721db0a244e1c1f997f26ab7fc03b115
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-11-27 16:19:39 +0530

    middleware for cors problem

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const feedRoutes = require('./routes/feed')
+const express = require("express");
+const bodyParser = require("body-parser");
+const feedRoutes = require("./routes/feed");
 const app = express();
 
-app.use(bodyParser.json()) // for json body
-app.use('/feed',feedRoutes) 
+app.use(bodyParser.json()); // for json body
 
-app.listen(8080, ()=>{
-    console.log("PORT is Listing at ", 8080)
-})
+
+// for cors problem
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // allow requests from any url/origin
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE"); // Allow All HTTP methods
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // allo
+  next()
+});
+
+app.use("/feed", feedRoutes);
+
+app.listen(8080, () => {
+  console.log("PORT is Listing at ", 8080);
+});
