4481e156a33d0565ddbb1fd869dd23b6eed85c1d
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-11-28 21:59:19 +0530

    Connected DB And write Schema for post.

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const feedRoutes = require("./routes/feed");
 const app = express();
 
 app.use(bodyParser.json()); // for json body
 
-
 // for cors problem
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); // allow requests from any url/origin
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE"); // Allow All HTTP methods
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  ); // Allow All HTTP methods
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // this allow client to setHeaders
-  next()
+  next();
 });
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080, () => {
-  console.log("PORT is Listing at ", 8080);
-});
+// mongoose.connect("mongodb://localhost:27017/messages").then((result) => {
+// console.log("Mongoose connectted", result);
+// app.listen(8080, () => {
+//     console.log("PORT is Listing at ", 8080);
+// });
+// });
+
+// mongoose.connect("mongodb://localhost:27017/messages")
+mongoose
+  .connect("mongodb://127.0.0.1:27017/messages")
+  .then((result) => {
+    console.log("Mongoose connected");
+    app.listen(8080, () => {
+      console.log("PORT is Listening at", 8080);
+    });
+  })
+  .catch((error) => {
+    console.error("Mongoose connection error:", error);
+  });
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,5 @@
-
-const { validationResult } = require('express-validator');
+const { validationResult } = require("express-validator");
+const Post = require("../models/post");
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -17,26 +17,29 @@ exports.getPosts = (req, res, next) => {
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req); // checking validation result.
-  if (!errors.isEmpty()) { // if body is not valid and has errors it will send some error response.
-    return res
-      .status(422)
-      .json({   
-        message: "Validation Failed, Entered Data Is Incorrect.",
-        errors: errors.array(),
-      });
+  if (!errors.isEmpty()) {
+    // if body is not valid and has errors it will send some error response.
+    return res.status(422).json({
+      message: "Validation Failed, Entered Data Is Incorrect.",
+      errors: errors.array(),
+    });
   }
   const { title, content } = req.body;
-  res.status(201).json({
-    // response 201 that's mean in this post is created without any error
-    message: "Post Created Successfully.",
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: {
-        name: "ikram",
-      },
-      createdAt: new Date(),
-    },
+  const post = new Post({
+    title,
+    content,
+    imageUrl: "images/duck.jpg",
+    creator: { name: "ikram" },
   });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        // response 201 that's mean in this post is created without any error
+        message: "Post Created Successfully.",
+        post: result,
+      });
+    })
+    .catch((e) => console.log(e));
 };
